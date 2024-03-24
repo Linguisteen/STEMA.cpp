47ec180c6feab194f29ccf3a468b8a8d8eeb4582
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2024-03-24 13:35:15 +0800

    set timeout for wizarmon

--- cpp/sketch/average.cpp
+++ cpp/sketch/average.cpp
@@ -4,11 +4,11 @@
  * 
  * 小兰同学也积极参加了本次演讲比赛，
  * 请你帮小兰计算一下她的最后得分。
- * 
+ *
  * @arg 8个整数作为分值，以空格分隔 (0 <= 分值 <= 100)
  * @result 平均分(保留两位小数)
  * 
- * @test
+ * @test 标准测试用例
  * input:
  * 50 90 55 78 52 68 66 93
  * output:
@@ -25,24 +25,41 @@
  * 60 70 80 90 91 92 93 94
  * output:
  * 86.00
+ * 
+ * @test
+ * input:
+ * 80 73 91 65 99 35 85 65
+ * output:
+ * 76.50
+ * 
+ * @test
+ * input:
+ * 58 100 45 67 82 56 73 73
+ * output:
+ * 68.17
  **/
 
 #include <iostream>
 
 int main(int argc, char* argv[]) {
-    int score;              // 当前分数
-    int max_score = 0;      // 最高分
-    int min_score = 100;    // 最低分
-    int total = 0;          // 总分, 初始化为0
-    float average;          // 平均分
-    int idx = 0;            // 已读分值数
+    int score;           // 当前分数
+    int max_score = 0;   // 最高分
+    int min_score = 100; // 最低分
+    int total = 0;       // 总分, 初始化为0
+    float average;       // 平均分
+    int idx = 0;         // 已读分值数
 
     while (idx < 8) {
         std::cin >> score;
-        total = total + score;
 
+        total = total + score;
+        
         if (score > max_score) {
             max_score = score;
+        }
+
+        if (score < min_score) {
+            min_score = score;
         }
 
         idx = idx + 1;
